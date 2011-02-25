f5088cb8053d189148cd71cddced5cb00c0691ea
PixelAlb <PixelAlb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-25 20:10:47 +0000

    version label update
    git-svn-id: https://plugins.svn.wordpress.org/maintenance-mode-notify/tags/2.0@351378 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- maintenance-mode-notify.php
+++ maintenance-mode-notify.php
@@ -3,7 +3,7 @@
 Plugin Name: Maintenance Mode Notifyer
 Plugin URI: http://pixelalb.ro
 Description: Shows a nice alert message to your users when your blog is in Maintenance Mode 
-Version: 1.0
+Version: 2.0
 Author: PixelAlb
 Author URI: http://pixelalb.ro
 */
@@ -13,13 +13,10 @@ activate_maintenance_mode();
 function activate_maintenance_mode() {
 	if (!is_admin()) {
 		$mesaj = get_option('mesaj');
-		$theme = get_option('theme');
-		$blink = get_option('blink');
-		if ($blink == "on") {$blinkval = "checked='checked'"; $blinkclass = "blink";}
 	if ($mesaj != "0") {
-	echo "<div class='alertMaintenance ". $theme ." ". $blinkclass ."'><span>";
+	echo "<div class='alertMaintenance'>";
 	 echo	get_option('mesaj');  
-	echo "<span></div>";
+	echo "</div>";
 }
 }
 }
@@ -27,11 +24,7 @@ function activate_maintenance_mode() {
 function my_css() {
 	echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') .'/wp-content/plugins/maintenance-mode-notify/css/alert.css" />' . "\n";
 }
-function bar_behavior() {
-	echo "<script type='text/javascript' src='" . get_bloginfo('wpurl') ."/wp-content/plugins/maintenance-mode-notify/js/functions.js'></script>" . "\n";
-}
 	add_action('wp_head', 'my_css');
-	add_action('wp_head', 'bar_behavior');
 
 
 
@@ -51,8 +44,6 @@ function mm_create_menu() {
 function register_mysettings() {
 	//register our settings
 	register_setting( 'mma-settings-group', 'mesaj' );
-	register_setting( 'mma-settings-group', 'theme' );
-	register_setting( 'mma-settings-group', 'blink' );
 }
 
 function mma_settings_page() {
@@ -64,26 +55,10 @@ function mma_settings_page() {
     <?php settings_fields( 'mma-settings-group' ); ?>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Message</th>
-        <td><textarea type="text" name="mesaj" style="width:300px; height:80px; overflow:auto"><?php echo get_option('mesaj'); ?></textarea></td>
+        <th scope="row">Mesajul</th>
+        <td><input type="text" name="mesaj" value="<?php echo get_option('mesaj'); ?>" /></td>
         </tr>
-		<tr valign="top">
-        <th scope="row">Look</th>
-        <td>
-			<select name="theme" style="width:300px;">
-        		<option value="<?php echo get_option('theme'); ?>"><?php echo get_option('theme'); ?></option>
-        		<option value="topYellowBar">topYellowBar</option>
-        		<option value="bottomYellowBar">bottomYellowBarr</option>
-        		<option value="fullScreenYellowBar">fullScreenYellowBar</option>
-        	</select>
-		</td>
-        </tr>
-         <tr valign="top">
-        <th scope="row">Should it blink?</th>
-        <td>
-	<?php if (get_option('blink') == "on") {echo '<input type="checkbox" checked="checked" name="blink"  />'; } else {echo '<input type="checkbox"  name="blink"  />';} ?>
-        </td>
-        </tr>
+         
        
     </table>
     
