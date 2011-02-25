7f6572bbccd6134ad36f90f2aa478f93e52ba5db
PixelAlb <PixelAlb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-25 20:44:00 +0000

    wp 3.1 compatibility issue fixed
    git-svn-id: https://plugins.svn.wordpress.org/maintenance-mode-notify/tags/2.0@351396 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- maintenance-mode-notify.php
+++ maintenance-mode-notify.php
@@ -8,15 +8,22 @@ Author: PixelAlb
 Author URI: http://pixelalb.ro
 */
 
-activate_maintenance_mode();
+
+// add_action('wp_head', 'my_css');
+// add_action('wp_head', 'bar_behavior');
 
 function activate_maintenance_mode() {
 	if (!is_admin()) {
 		$mesaj = get_option('mesaj');
+		$theme = get_option('theme');
+		$blink = get_option('blink');
+		if ($blink == "on") {$blinkval = "checked='checked'"; $blinkclass = "blink";}
 	if ($mesaj != "0") {
-	echo "<div class='alertMaintenance'>";
+	echo "<div class='alertMaintenance ". $theme ." ". $blinkclass ."'><span>";
 	 echo	get_option('mesaj');  
-	echo "</div>";
+	echo "<span></div>";
+		echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') .'/wp-content/plugins/maintenance-mode-notify/css/alert.css" />' . "\n";
+			echo "<script type='text/javascript' src='" . get_bloginfo('wpurl') ."/wp-content/plugins/maintenance-mode-notify/js/functions.js'></script>" . "\n";
 }
 }
 }
@@ -24,7 +31,10 @@ function activate_maintenance_mode() {
 function my_css() {
 	echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') .'/wp-content/plugins/maintenance-mode-notify/css/alert.css" />' . "\n";
 }
-	add_action('wp_head', 'my_css');
+function bar_behavior() {
+	echo "<script type='text/javascript' src='" . get_bloginfo('wpurl') ."/wp-content/plugins/maintenance-mode-notify/js/functions.js'></script>" . "\n";
+}
+
 
 
 
@@ -44,6 +54,8 @@ function mm_create_menu() {
 function register_mysettings() {
 	//register our settings
 	register_setting( 'mma-settings-group', 'mesaj' );
+	register_setting( 'mma-settings-group', 'theme' );
+	register_setting( 'mma-settings-group', 'blink' );
 }
 
 function mma_settings_page() {
@@ -55,10 +67,26 @@ function mma_settings_page() {
     <?php settings_fields( 'mma-settings-group' ); ?>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Mesajul</th>
-        <td><input type="text" name="mesaj" value="<?php echo get_option('mesaj'); ?>" /></td>
+        <th scope="row">Message</th>
+        <td><textarea type="text" name="mesaj" style="width:300px; height:80px; overflow:auto"><?php echo get_option('mesaj'); ?></textarea></td>
         </tr>
-         
+		<tr valign="top">
+        <th scope="row">Look</th>
+        <td>
+			<select name="theme" style="width:300px;">
+        		<option value="<?php echo get_option('theme'); ?>"><?php echo get_option('theme'); ?></option>
+        		<option value="topYellowBar">topYellowBar</option>
+        		<option value="bottomYellowBar">bottomYellowBarr</option>
+        		<option value="fullScreenYellowBar">fullScreenYellowBar</option>
+        	</select>
+		</td>
+        </tr>
+         <tr valign="top">
+        <th scope="row">Should it blink?</th>
+        <td>
+	<?php if (get_option('blink') == "on") {echo '<input type="checkbox" checked="checked" name="blink"  />'; } else {echo '<input type="checkbox"  name="blink"  />';} ?>
+        </td>
+        </tr>
        
     </table>
     
